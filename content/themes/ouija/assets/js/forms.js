5eaea685e84852ee4bd07d74d4aaa5448e62c3bf
Ethan Herr <eherr@reonomy.com>
2016-08-10 23:39:05 -0400

    make new route for forms

---
+++ content/themes/ouija/assets/js/forms.js
@@ -0,0 +1,28 @@
+// Ouija Forms JS
+
+(function ($, undefined) {
+  "use strict";
+
+  var $document = $(document);
+
+  $document.ready(function () {
+
+    $('#submit').on('click', function(e){
+      e.preventDefault();
+      console.log('pressy!');
+
+      $.post('/ghost/api/v0.1/apply', {
+        wank: 'wanky',
+        name: $('input[name="name"]').val(),
+        email: $('input[name="email"]').val(),
+        phone: $('input[name="phone"]').val()
+      }).done(function(a){
+        console.log('yah', a);
+      }).fail(function(a){
+        console.log('no', a);
+      });
+    });
+
+  });
+
+})(jQuery);
